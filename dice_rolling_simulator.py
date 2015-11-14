27671368afa86dce9de03295e386e00de7b3a267
Abishek Goda <goda.abishek@gmail.com>
2015-11-14 10:09:13 +0800

    Updated README; adds code for the first mini project.

---
+++ dice_rolling_simulator.py
@@ -0,0 +1,28 @@
+## Dice Rolling Simulator
+
+from random import Random
+
+def roll_dice(random_simulator, choices) :
+
+	return random_simulator.choice(choices)
+
+def run_dice_rolling_simulator() :
+
+	r = Random()
+	choices = (1,2,3,4,5,6)
+	print "Dice chose: ", roll_dice(r, choices)
+	ui = None
+	while not ui :
+		ui = raw_input('Do you want to roll again? (y/n) ')
+		if ui == 'y' :
+			run_dice_rolling_simulator()
+		elif ui == 'n':
+			break
+		else :
+			ui = None
+
+
+if __name__ == '__main__' :
+
+	run_dice_rolling_simulator()
+
